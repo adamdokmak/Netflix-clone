091b8eba128f702d0b4ad64267e2497e10dc09c1
Adam <adamdokmak04@gmail.com>
2023-10-06 21:46:22 +0100

    initial commit, nav bar, banner image and styling, filtered rows.

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'Login - Netflix',
+}
+
+export default function page() {
+    return (
+        <>
+            a
+        </>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,10 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+      require('tailwindcss-textshadow'),
+      require('tailwind-scrollbar-hide'),
+      require('tailwind-scrollbar'),
+  ],
 }
 export default config
